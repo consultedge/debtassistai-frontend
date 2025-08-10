98991c047bf08786c728bacd72e9ac4c5153e1a6
consultedge <sammyceg@gmail.com>
2025-08-10 19:12:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import './styles.css';
 function App() {
   const [messages, setMessages] = useState([]);
 
-  const suggestions = [
-    'How can I improve my credit score?',
-    'Explain my debt repayment options',
-    'Help me understand my rights'
-  ];
-
   // On first load, show welcome message
   useEffect(() => {
     setMessages([
@@ -29,29 +23,10 @@ function App() {
     // TODO: Call your API here (sendMessage) and push bot response
   };
 
-  // Suggestion chip click
-  const handleSuggestionClick = (suggestion) => {
-    handleNewMessage({ from: 'user', text: suggestion });
-  };
-
   return (
     <div className="app-container">
       <h1>DebtAssistAI</h1>
-
       <MessageList messages={messages} />
-
-      <div className="suggestions">
-        {suggestions.map((s, idx) => (
-          <button
-            key={idx}
-            className="suggestion-chip"
-            onClick={() => handleSuggestionClick(s)}
-          >
-            {s}
-          </button>
-        ))}
-      </div>
-
       <ChatForm onNewMessage={handleNewMessage} />
     </div>
   );
